f47ae52d7c8a37b6ef781f64f540f7159c1d7e1e
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-25 04:54:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    final = {}
+  names = []
+
+  #Create 'names' array with all unique names of pigeons
+  data.each do |c_g_l, attribute|
+    attribute.each do |feature, name|
+      name.each do |firstname|
+        names << (firstname) if !names.include?(firstname)
+      end
+    end
+  end
+  
+  names.each do |nam|
+    final[nam] = Hash.new {|key,value| key[value] = []}
+    data.each do |c_g_l, attribute|
+      final[nam][c_g_l]
+      attribute.each do |feature, namearray|
+        namearray.each do |uname|
+          final[uname][c_g_l] << feature.to_s if uname == nam
+        end
+      end   
+    end
+  end
+  final
 end
